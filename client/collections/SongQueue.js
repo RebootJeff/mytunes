3405c521e8cb7dc4d6fc3084ee5e83e5eeffff4a
Hack Reactor Students <github@hackreactor.com>
2013-10-09 22:13:11 -0700

    Passes all tests in SongQueueSpec and SongQueueViewSpec

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,28 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add', function(){
+      this.models.length === 1 && this.playFirst();
+    });
+    this.on('ended', this.removeFirst);
+    this.on('dequeue', this.dequeue);
+  },
+
+  playFirst: function(){
+    this.first().play();
+  },
+
+  removeFirst: function(){
+    this.remove(this.first());
+    if(this.models.length){
+      this.playFirst();
+    }
+  },
+
+  dequeue: function(song){
+    //if(_(this.models).contains(song) ){   // return to this
+       this.remove(song);
+    //}
   }
 
 });
